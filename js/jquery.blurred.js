7908f2f19bff3a70a639d506b01206b5722c8f8f
Jeremy North-Lewis <northewis.jeremy@gmail.com>
2015-02-24 16:59:40 -0500

    Set up the basic structure of the page. Added an image.

--- js/jquery.blurred.js
+++ js/jquery.blurred.js
@@ -1,1 +1,5 @@
-//select background image of header
+//use $(window).scrollTop(); to figure out how far down the user has scrolled.
+
+//use $(window).height(); to get the height of the user's window.
+
+//Divide the $(window).height by the .scrollTop to make it into a percentage.
