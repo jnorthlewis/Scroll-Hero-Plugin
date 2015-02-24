bfb14071be33a277fefe6796cb230a1fb1e97137
Jeremy North-Lewis <northewis.jeremy@gmail.com>
2015-02-24 17:46:13 -0500

    Added some math variables to the JS file.

--- js/jquery.blurred.js
+++ js/jquery.blurred.js
@@ -3,3 +3,23 @@
 //use $(window).height(); to get the height of the user's window.
 
 //Divide the $(window).height by the .scrollTop to make it into a percentage.
+
+$(window).on('scroll',function(){
+
+	var $windowHeight = $(window).height();
+	var $scrollHeight = $(window).scrollTop();
+
+	var $scrollPercentage =  ($scrollHeight / $windowHeight);
+
+	var maxBlur = 10; // the max blur it can be
+	var blurRatio = 100 / maxBlur;
+	var blurAmount = $scrollPercentage * blurRatio; 
+	// for every 10% scrolled, blur it 1px
+
+	console.log("it should be blured " + blurAmount);
+	$('.top').css({
+		'-webkit-filter' : 'blur('+blurAmount+'px)'
+	});
+	console.log("blur ratio is  " + blurRatio);
+
+});
