b801d089cf4ea38e3dd5bbeb5e8dc240564584c0
Jeremy North-Lewis <northewis.jeremy@gmail.com>
2015-02-25 16:56:37 -0500

    Rearranged the html and added a new filter to the CSS

--- js/jquery.blurred.js
+++ js/jquery.blurred.js
@@ -8,18 +8,29 @@ $(window).on('scroll',function(){
 
 	var $windowHeight = $(window).height();
 	var $scrollHeight = $(window).scrollTop();
-
-	var $scrollPercentage =  ($scrollHeight / $windowHeight);
+	var scrollPercentage =  ($scrollHeight / $windowHeight);
 
 	var maxBlur = 10; // the max blur it can be
 	var blurRatio = 100 / maxBlur;
-	var blurAmount = $scrollPercentage * blurRatio; 
+	var blurAmount = scrollPercentage * blurRatio; 
 	// for every 10% scrolled, blur it 1px
 
+	//var maxDark = 0; // the max blur it can be
+	//var darkRatio = 0.5 / maxDark;
+	//var darkAmount = scrollPercentage * darkRatio; 
+	// for every 10% scrolled, darken it 1%
+	console.log(scrollPercentage);
+
+	var brightnessPercentage = 100 - Math.round(scrollPercentage * 100);
+
 	console.log("it should be blured " + blurAmount);
+	console.log("Brightness is ", brightnessPercentage);
+
 	$('.top').css({
-		'-webkit-filter' : 'blur('+blurAmount+'px)'
+		'-webkit-filter' : 'blur('+blurAmount+'px)',
+		'-webkit-filter': 'brightness('+ brightnessPercentage + '%)'  
 	});
+
 	console.log("blur ratio is  " + blurRatio);
 
 });
