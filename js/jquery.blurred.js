421eb7f20860a6218bb4b01c6bc5c8514b45973c
Jeremy North-Lewis <northewis.jeremy@gmail.com>
2015-02-26 14:35:38 -0500

    Added an opacity filter to the JS file.

--- js/jquery.blurred.js
+++ js/jquery.blurred.js
@@ -6,31 +6,36 @@
 
 $(window).on('scroll',function(){
 
+	//Getting the scroll percentage
 	var $windowHeight = $(window).height();
 	var $scrollHeight = $(window).scrollTop();
 	var scrollPercentage =  ($scrollHeight / $windowHeight);
 
+	//blur filter
 	var maxBlur = 10; // the max blur it can be
 	var blurRatio = 100 / maxBlur;
-	var blurAmount = scrollPercentage * blurRatio; 
-	// for every 10% scrolled, blur it 1px
-
-	//var maxDark = 0; // the max blur it can be
-	//var darkRatio = 0.5 / maxDark;
-	//var darkAmount = scrollPercentage * darkRatio; 
-	// for every 10% scrolled, darken it 1%
-	console.log(scrollPercentage);
-
+	var blurAmount = scrollPercentage * blurRatio; //for every 10% scrolled, blur it 1px
+	
+	//brightness filter
 	var brightnessPercentage = 100 - Math.round(scrollPercentage * 100);
+	
+	//greyscale filter
+	var grayscalePercentage = 0 + Math.round(scrollPercentage * 100);
 
-	console.log("it should be blured " + blurAmount);
-	console.log("Brightness is ", brightnessPercentage);
+	//opacity filter
+	var opacityFilter =  100 - Math.round(scrollPercentage * 100);
+	//var opacityFilter;
+	console.log('The opacity is ' + opacityFilter); 
+
+	//console.log("it should be blured " + blurAmount);
+	//console.log("Brightness is ", brightnessPercentage);
 
 	$('.top').css({
-		'-webkit-filter' : 'blur('+blurAmount+'px)',
-		'-webkit-filter': 'brightness('+ brightnessPercentage + '%)'  
+		'-webkit-filter' : 'blur('+blurAmount+'px) brightness('+ brightnessPercentage + '%) grayscale('+ grayscalePercentage + '%) opacity('+ opacityFilter + '%) '
+		//'-webkit-filter': 'brightness('+ brightnessPercentage + '%)',  
+		//'-webkit-filter': 'grayscale('+ grayscalePercentage + '%)'  
 	});
 
-	console.log("blur ratio is  " + blurRatio);
+	//console.log("blur ratio is  " + blurRatio);
 
 });
